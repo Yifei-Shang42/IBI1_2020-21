65216e0f2f0b181e04c670e117855c275b28ba69
Yifei Shang <3200111314@zju.edu.cn>
2021-03-16 05:57:24 +0100

    edit 2 python files in Practical6

--- Practical6/coronavirus_rates.py
+++ Practical6/coronavirus_rates.py
@@ -1,19 +1,17 @@
 # This code compares coronavirus
 # infection rates across countries, pseudocode included in comments
-
-# import required modules
 import numpy as np
 import matplotlib.pyplot as plt
 # construct a dictionary to store data
-dict_of_cases = {"USA":29862124, "India":11285561,
-                 "Brazil":11205972, "Russia":4360823,
-                 "UK":4234924}
-# print the dictionary
+dict_of_cases = {"USA": 29862124, "India": 11285561,
+                 "Brazil": 11205972, "Russia": 4360823,
+                 "UK": 4234924}
+# print the dictionary as output
 print(dict_of_cases)
 # turn values & keys in dict_of_cases into 2 arrays
 data = np.array([i for i in dict_of_cases.values()])
 labels = np.array([j for j in dict_of_cases.keys()])
-# draw the pie chart
-plt.pie(data, labels=labels)
+# draw the pie chart as output
+plt.pie(data, labels=labels, shadow=True)
 plt.title("Coronavirus Infection Rates across Countries")
 plt.show()
--- Practical6/list_manipulation.py
+++ Practical6/list_manipulation.py
@@ -1,13 +1,13 @@
 # list manipulation
-# pseudocodes included in comments
+# Pseudo-codes included in comments
 
 # import required modules
 import numpy as np
 import matplotlib.pyplot as plt
 # create 2 lists
-gene_lengths = [9410,394141,4442,105338,19149,76779,
-              126550,36296,842,15981]
-exon_counts = [51,1142,42,216,25,650,32533,57,1,523]
+gene_lengths = [9410, 394141, 4442, 105338, 19149, 76779,
+                126550, 36296, 842, 15981]
+exon_counts = [51, 1142, 42, 216, 25, 650, 32533, 57, 1, 523]
 # convert lists into arrays
 lengths_array = np.array(gene_lengths)
 counts_array = np.array(exon_counts)
@@ -20,4 +20,3 @@ explode = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0.1)
 plt.pie(sorted_average_length_array, shadow=True, explode=explode)
 plt.title("Average Exon Lengths")
 plt.show()
-
