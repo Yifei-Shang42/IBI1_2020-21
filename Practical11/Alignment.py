e49d8a34a8f793926281fef8a173636f03541c06
Yifei-Shang42 <yifeis.20@intl.zju.edu.cn>
2021-04-21 13:03:17 +0800

    edit Alignment.py

--- Practical11/Alignment.py
+++ Practical11/Alignment.py
@@ -24,7 +24,7 @@ sod2_human = read_fasta("C:\\Users\\surface\\Documents\\GitHub"
                         "\\IBI1_2020-21\\Practical11\\SOD2_human.fa")
 
 sod2_mouse = read_fasta("C:\\Users\\surface\\Documents\\GitHub"
-                        "\\IBI1_2020-21\\Practical11\\SOD2_human.fa")
+                        "\\IBI1_2020-21\\Practical11\\SOD2_mouse.fa")
 
 random_seq = read_fasta("C:\\Users\\surface\\Documents\\GitHub"
                         "\\IBI1_2020-21\\Practical11\\RandomSeq.fa")
@@ -50,14 +50,17 @@ def my_blosum_62(seq1, seq2):
             The alignment score of the two proteins using BLOSUM62 matrix
         """
         score = 0
+        match_count = 0
         for i in range(len(s1)):
             amino_pair = (s1[i], s2[i])
+            if s1[i] == s2[i]:
+                match_count += 1
             if amino_pair not in blosum62:
                 revered_pair = (s2[i], s1[i])
                 score += blosum62[revered_pair]
             else:
                 score += blosum62[amino_pair]
-        return score
+        return score, match_count/len(s1)
 
     # if sequences are of the same length, one alignment will be sufficient
     if len(seq1) == len(seq2):
@@ -68,33 +71,35 @@ def my_blosum_62(seq1, seq2):
 
     else:
         diff = abs(len(seq1)-len(seq2))
-        score_list = []
+        result_list = []
         if len(seq1) < len(seq2):
             for i in range(diff+1):
-                score = one_alignment(seq1, seq2[i:i+len(seq1)])
-                score_list.append(score)
+                score = one_alignment(seq1, seq2[i:i+len(seq1)])[0]
+                percentage = one_alignment(seq1, seq2[i:i+len(seq1)])[1]
+                result_list.append([score, percentage])
 
         else:
             for i in range(diff+1):
-                score = one_alignment(seq2, seq1[i:i+len(seq2)])
-                score_list.append(score)
+                score = one_alignment(seq2, seq1[i:i+len(seq2)])[0]
+                percentage = one_alignment(seq2, seq1[i:i+len(seq2)])[1]
+                result_list.append([score, percentage])
 
-        return max(score_list)
+        score_list = [result[0] for result in result_list]
+        percentage_list = [result[1] for result in result_list]
+        for i in range(len(result_list)):
+            if score_list[i] == max(score_list):
+                return score_list[i], percentage_list[i]
 
-
-# human-mouse alignment
-human_mouse_score = my_blosum_62(sod2_human[1], sod2_mouse[1])
-
-# human-random alignment
-human_random_score = my_blosum_62(sod2_human[1], random_seq[1])
-
-# mouse-random alignment
-mouse_random_score = my_blosum_62(sod2_mouse[1], random_seq[1])
-
-# Print Output
+# Print Outputs
 def gen_output(fasta1, fasta2):
     for item in fasta1:
         print(item)
     for item in fasta2:
         print(item)
-    print(my_blosum_62(fasta1[1], fasta2[1]))
+    print("Score: ", my_blosum_62(fasta1[1], fasta2[1])[0], end=" ")
+    print("Percentage: ", my_blosum_62(fasta1[1], fasta2[1])[1])
+
+gen_output(sod2_human, sod2_mouse)
+gen_output(sod2_human, random_seq)
+gen_output(sod2_mouse, random_seq)
+
