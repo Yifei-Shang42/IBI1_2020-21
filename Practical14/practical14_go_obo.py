b1ce6cfc7ca63193f17cca589bfd478d2647b5ce
Yifei-Shang42 <yifeis.20@intl.zju.edu.cn>
2021-05-13 01:31:15 +0800

    correct practical14_go_obo.py

--- Practical14/practical14_go_obo.py
+++ Practical14/practical14_go_obo.py
@@ -1,19 +1,10 @@
 # Practical 14: Working with Information
 
-"""
-All childNodes of the macromolecule-associated terms are counted
-DUPLICATES NOT REMOVED
-Therefore, childNodes numbers are larger than total term number
-"""
-
 # import modules
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import matplotlib.pyplot as plt
 import numpy as np
-
-# define file path (absolute path)
-path = "C:\\Users\\surface\\Documents\\Github\\IBI1_2020-21\\Practical14\\go_obo.xml"
 
 # assistant function for parsing xml file
 def parse_xml(path):
@@ -32,43 +23,105 @@ def get_primary_matches(terms, molecule_name):
             match_list.append(term)
     return match_list
 
-# assistant function for all the children (including the parents from previous function, parents are removed in the next function)
-def getAllChildren(term_id_dict, elements):
-    allChildren = []
-    for element in elements:
-        is_a = element.getElementsByTagName("is_a")
-        # basic condition: if current node has no children, total + 1
-        if not is_a:
-            allChildren.append(element)
-        # get all children of the current node
+# 2 assistant classes: Vertex & Graph to structurize terms
+class Vertex:
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+
+    def addNeighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
+
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' \
+               + str([x.id for x in self.connectedTo])
+
+    def getConnection(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self, nbr):
+        return self.connectedTo[nbr]
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self, key):
+        self.numVertices += 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+
+    def getVertex(self, n):
+        if n in self.vertList:
+            return self.vertList[n]
         else:
-            children_id = [child.childNodes[0].data for child in is_a]
-            children = []
-            for id in children_id:
-                children.append(term_id_dict[id])
-            # recursive method to count all children
-            allChildren.append(element)
-            allChildren += getAllChildren(term_id_dict, children)
+            return None
+
+    def __contains__(self, item):
+        return item in self.vertList
+
+    def addEdge(self, f, t, cost=0):
+        if f not in self.vertList:
+            nv = self.addVertex(f)
+        if t not in self.vertList:
+            nv = self.addVertex(t)
+        self.vertList[t].addNeighbor(self.vertList[f], cost)
+
+    def getVertices(self):
+        return self.vertList.keys()
+
+    def __iter__(self):
+        return iter(self.vertList.values())
+
+# assistant function to build tree of terms
+def buildTree(terms):
+    tree = Graph()
+    for term in terms:
+        id = term.getElementsByTagName("id")[0].childNodes[0].data
+        is_a = [child.childNodes[0].data for child in term.getElementsByTagName("is_a")]
+        for child in is_a:
+            tree.addEdge(id, child)
+    return tree
+
+# assistant recursive function to find all childNodes as vertices
+def getAllChildren(tree, vertices):
+    allChildren = []
+    for vertex in vertices:
+        if vertex:
+            if not vertex.connectedTo:
+                allChildren.append(vertex)
+            else:
+                children = [vtx for vtx in vertex.connectedTo]
+                allChildren.append(vertex)
+                allChildren += getAllChildren(tree, children)
     return allChildren
 
 # main function outputting the final result
-def count_childNodes_macromolecules_xml(terms, molecule_name):
-    term_id_dict = {}
-    for term in terms:
-        id = term.getElementsByTagName("id")[0].childNodes[0].data
-        term_id_dict[id] = term
+def count_childNodes_macromolecules_xml(tree, molecule_name):
     match_list = get_primary_matches(terms, molecule_name)
-    count = len(getAllChildren(term_id_dict, match_list)) - len(match_list)
+    vertices_list = [tree.getVertex(term.getElementsByTagName("id")[0].childNodes[0].data) for term in match_list]
+    count = len(list(set(getAllChildren(tree, vertices_list))))
     return count
+
+# define file path (absolute path)
+path = "C:\\Users\\surface\\Documents\\Github\\IBI1_2020-21\\Practical14\\go_obo.xml"
 
 # Parse the xml file
 terms = parse_xml(path)
 
+# build tree
+tree = buildTree(terms)
+
 # Calculate & print the data
-dna = count_childNodes_macromolecules_xml(terms, "DNA")
-rna = count_childNodes_macromolecules_xml(terms, "RNA")
-protein = count_childNodes_macromolecules_xml(terms, "protein")
-carbo = count_childNodes_macromolecules_xml(terms, "carbohydrate")
+dna = count_childNodes_macromolecules_xml(tree, "DNA")
+rna = count_childNodes_macromolecules_xml(tree, "RNA")
+protein = count_childNodes_macromolecules_xml(tree, "protein")
+carbo = count_childNodes_macromolecules_xml(tree, "carbohydrate")
 
 print("Number of childNodes of all DNA-associated terms: {}".format(dna))
 print("Number of childNodes of all RNA-associated terms: {}".format(rna))
